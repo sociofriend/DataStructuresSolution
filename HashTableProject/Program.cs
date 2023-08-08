d4caf06220cf0d88c138c0f886a767766430f056
sociofriend <lilit_avdalyan@yahoo.com>
2023-08-08 19:52:44 +0400

    Hashing logic implemented, repetative input.

--- HashTableProject/Program.cs
+++ HashTableProject/Program.cs
@@ -5,13 +5,19 @@ public class Program
 {
     static void Main(string[] args)
     {
-        //optional string type input
-        string input = "Once upon a time t";
-        
+        while (true)
+        {
+            HashText();
+        }
+    }
+
+    private static void HashText()
+    {
+        Console.Write("Input text for hashing: ");
         //run the hashing logic
-        HashingLogic hashText = new HashingLogic(input);
-        
-        //print final result of the calculation.
-        Console.WriteLine(hashText.FinalCode);
+        HashingLogic hashText = new HashingLogic(Console.ReadLine());
+
+        // print final result of the calculation.
+        Console.WriteLine("Your output: " + hashText.FinalCode);
     }
 }
